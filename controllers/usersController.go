7ab5e9b5b78f0656a343515908ef891243e9b668
jicodes <liu.ji1@northeastern.edu>
2024-02-07 17:31:07 -0800

    add controllers for login auth which implemented with golang-jwt package v4

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,11 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jicodes/go-jwt/initializers"
 	"github.com/jicodes/go-jwt/models"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func Signup(c *gin.Context) {
@@ -44,8 +47,59 @@ func Signup(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{}) //200
 }
 
+func Login(c *gin.Context) {
+	// Get the email and password from the request body
+	var body struct {
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
 
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{ //400
+			"error": "Failed to read request body",
+		}) 
+		return
+	}
 
+	// look up requeted user in the database
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
 
+	if user.ID == 0 {
+		c.JSON(http.StatusUnauthorized, gin.H{ //401
+			"error": "Invalid credentials",
+		})
+		return
+	}
+	// compare sent in password with the hashed password in the database
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{ //401
+			"error": "Invalid credentials",
+	})
+		return
+	}
+	// if the password is correct, generate a JWT 
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID, // subject
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(), // expiration time
+	
+	})
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
 
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{ //400
+			"error": "Failed to generate token",
+		})
+		return
+	}
+	
+	// set the token in a cookie
+	c.SetSameSite(http.SameSiteLaxMode) // set the SameSite attribute to Lax, which means the cookie will be sent with same-site requests, and with cross-site top-level navigations, to protect against cross-site request forgery attacks
+	c.SetCookie("Authorization", tokenString, 60*60*24*30, "", "", false, true)
 
+	// send it back to the client in the response 
+	c.JSON(http.StatusOK, gin.H{"token": tokenString}) //200
+
+}
--- main.go
+++ main.go
@@ -14,7 +14,10 @@ func init () {
 
 func main() {
 	r := gin.Default()
+
 	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	
 	r.Run()
 
 }
