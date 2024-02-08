1939e5b3833464de2e8c884f348d494328679f28
jicodes <liu.ji1@northeastern.edu>
2024-02-07 18:23:05 -0800

    add middleware for authorization

---
+++ middleware/requireAuthz.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/jicodes/go-jwt/initializers"
+	"github.com/jicodes/go-jwt/models"
+)
+
+func RequireAuthz(c *gin.Context) {
+	// Get the token from the cookie named "Authorization of the request
+	tokenString, err := c.Cookie("Authorization")
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+	// Decode the token/validate it 
+	// Parse takes the token string and a function for looking up the key. The function should return the key for the given token.
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret")
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// Check if the token is valid (exp)
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		} 
+		// Find the user with token's subject(sub)
+		var user models.User
+		initializers.DB.First(&user, "id = ?", claims["sub"])
+		if user.ID == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		// Attach the user to the context
+		c.Set("user", user)
+		// Continue
+		c.Next()
+
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
