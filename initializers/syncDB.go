d8b2f9917164756c4ca956c4bfcbcbe55db5b387
jicodes <liu.ji1@northeastern.edu>
2024-02-07 20:46:50 -0800

    optimize err handling of syncDB initializer

--- initializers/syncDB.go
+++ initializers/syncDB.go
@@ -1,7 +1,14 @@
 package initializers
 
-import "github.com/jicodes/go-jwt/models"
+import (
+	"log"
+
+	"github.com/jicodes/go-jwt/models"
+)
 
 func SyncDB () {
 	DB.AutoMigrate(&models.User{})
+	if err := DB.AutoMigrate(&models.User{}); err != nil {
+			log.Fatal("Error migrating the database schema:", err)
+	}
 }
